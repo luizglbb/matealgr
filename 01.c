097ae7931c17581ec76bf43314ee02f3869649f8
Luiz Gabriel <luizgabriel8123@outlook.com>
2025-09-23 10:00:52 -0300

    Update and rename 1.c to 01.c

---
+++ 01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+    int a,b, n;
+    printf("digite um valor A: ");
+    scanf("%d",&a);
+    
+     printf("digite um valor B: ");
+    scanf("%d",&b);
+    
+    n = a * b;
+    
+    printf("A multiplicasao de %d e %d = %d",a,b,n);
+    return 0;
+    
+}
