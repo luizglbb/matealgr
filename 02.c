e501ac7662404f399206a6f08061f0ee390ddf4a
Luiz Gabriel <luizgabriel8123@outlook.com>
2025-09-23 10:15:07 -0300

    Create 02.c

---
+++ 02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    
+    int numero, antecessor, sucessor;
+
+    
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numero);
+
+    antecessor = numero - 1;
+    sucessor = numero + 1;
+
+
+    printf("O antecessor de %d eh: %d\n", numero, antecessor);
+    printf("O sucessor de %d eh: %d\n", numero, sucessor);
+
+    return 0; 
+}
