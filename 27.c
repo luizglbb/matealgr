ccd343fc326d9ff9c75e5c7930a858977dd51500
Luiz Gabriel <luizgabriel8123@outlook.com>
2025-11-12 21:34:30 -0300

    Clean up comments in 27.c
    Removed unnecessary comments and header from the file.

---
+++ 27.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+// questão 25
+int main()
+{
+    int i, res;
+    
+    for (i=15; i<=30; i++){
+        res = i * i;
+            printf ("%d * %d = %d\n", i, i, res);
+        
+   
+    }
+
+    return 0;
+}
