24a4126d336c3904d10f883bd82a6b9616ddf91b
Luiz Gabriel <luizgabriel8123@outlook.com>
2025-11-12 21:39:17 -0300

    Clean up code in 25.c
    Removed unnecessary comments and cleaned up code.

--- 25.c
+++ 25.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 // questão 25
 int main()
@@ -16,6 +8,5 @@ int main()
        
     printf("%d ", i);
     }
- // não entendi se era para incluir ou deixar o 14 e o 30, então eu deixei
     return 0;
 }
